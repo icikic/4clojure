8ce8162ce792bddf2ad63fb448072971efc30f8c
CAF dev <caf@ocs-caf-dev.(none)>
2015-01-13 18:17:26 +0000

    Problem 22 solved

--- src/for_clojure/core.clj
+++ src/for_clojure/core.clj
@@ -1,6 +1,23 @@
 (ns for-clojure.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;; #22
+(defn my-count
+  "A function which returns the total number of elements in a sequence. Map/reduce version"
+  [coll]
+  (reduce + (map (fn [_] 1) coll)))
+
+(defn my-count-2
+  "A function which returns the total number of elements in a sequence. Map/reduce + constantly version"
+  [coll]
+  (reduce + (map (constantly 1) coll)))
+
+(defn my-count-3
+  "A function which returns the total number of elements in a sequence. Recur version"
+  [coll]
+  (loop [c coll
+         cnt 0]
+    (if (seq c)
+      (recur (rest c) (inc cnt))
+      cnt)))
+
+;; #23
