71f5ba23ebfda64a64cd71fdf1926b80bd60bd7a
CAF dev <caf@ocs-caf-dev.(none)>
2015-01-18 22:08:23 +0000

    Solved 28,29,30

--- src/for_clojure/core.clj
+++ src/for_clojure/core.clj
@@ -21,3 +21,49 @@
       cnt)))
 
 ;; #23
+(defn my-rseq
+  "A function which reverses a sequence."
+  [seq]
+  (reduce conj '() seq))
+
+;; #24
+(defn my-sum
+  "A function which returns the sum of a seqence of numbers"
+  [seq]
+  (reduce + seq))
+
+;; #25
+(defn filter-odd
+  "A function which returns only the odd numbers from a sequence"
+  [seq]
+  (filter odd? seq))
+
+;; #26
+(defn fibonacci-seq
+  "A function which returns the first X fibonacci numbers."
+  [x]
+  (take x (map first (iterate (fn [[a b]] [b (+ a b)]) [1 1]))))
+
+;; #27
+(defn palindrom
+  "A function which returns true if the given sequence is a palindrome."
+  [s]
+  (= (seq s) (reverse (seq s))))
+
+;; #28
+(defn my-flatten
+  "A function which flattens a sequence."
+  [x]
+  (filter (complement sequential?) (tree-seq sequential? seq x)))
+
+;; #29
+(defn caps-only
+  "A function which takes a string and returns a new string containing only the capital letters."
+  [s]
+  (apply str (re-seq #"[A-Z]+" s)))
+
+;; #30
+(defn compress-seq
+  "A function which removes consecutive duplicates from a sequence."
+  [s]
+  (map first (partition-by identity s)))
--- test/for_clojure/core_test.clj
+++ test/for_clojure/core_test.clj
@@ -2,6 +2,15 @@
   (:require [clojure.test :refer :all]
             [for-clojure.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(deftest caps-only-test
+  (testing "Problem 29"
+    (is (= (caps-only "HeLlO, WoRlD!") "HLOWRD"))
+    (is (empty? (caps-only "nothing")))
+    (is (= (caps-only "$#A(*&987Zf") "AZ"))))
+
+(deftest compress-seq-test
+  (testing "Problem 30"
+    (is (= (apply str (compress-seq "Leeeeeerrroyyy")) "Leroy"))
+    (is (= (compress-seq [1 1 2 3 3 2 2 3]) '(1 2 3 2 3)))
+    (is (= (compress-seq [[1 2] [1 2] [3 4] [1 2]]) '([1 2] [3 4] [1 2])))))
